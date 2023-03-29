9b93df6f28763c9afbba1d94c23bfff2c5739e62
hazel1814 <terilolots@gmail.com>
2023-03-29 23:09:06 +0530

    Making th items visible

---
+++ src/Restaurant_project/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+
+import classes from './Header.module.css'
+import meals from '../../assets/meals.jpg'
+import { HeaderCartButton } from './HeaderCartButton'
+
+
+export const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>Meals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt='A table full of delicious food' />
+            </div>
+        </>
+    )
+}
---
+++ src/Restaurant_project/Components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import { MealsSummary } from "./MealsSummary"
+import { AvailableMeals } from "./AvailableMeals"
+
+export const Meals = () => {
+  return (
+    <>
+    <MealsSummary />
+    <AvailableMeals />
+    </>
+  )
+}
